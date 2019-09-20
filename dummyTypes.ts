de56ca0117e88772105b0ca22744845fddd7b81c
dadada <nuhhunnuhhun@gmail.com>
2019-09-20 12:10:09 +0900

    Dummy-Name-#16
    - Add dummy name template
    - Enable to generate dummy Jp Name

--- dummyTypes.ts
+++ dummyTypes.ts
@@ -1,4 +1,6 @@
 export const dummyTypes = {
   DESCRIPTION: 'description',
-  NUMBER: 'number'
+  NUMBER: 'number',
+  FULLNAME: 'fullName',
+  FULLNAMEKANA: 'fullNameKana'
 } as const
---
+++ pages/api/dummyData/getRandomJpNameAndKana.ts
@@ -0,0 +1,20 @@
+import dummyNameTemplate from "../../../data/dummyJpNameTemplate.json"
+
+export interface NameAndKana {
+  familyName: string,
+  firstName: string,
+  familyNameKana: string,
+  firstNameKana: string
+}
+
+export default (): NameAndKana => {
+  const dummyLength = dummyNameTemplate.length;
+  const familyDummyIndex = Math.floor(Math.random() * (dummyLength - 1));
+  const firstDummyIndex = Math.floor(Math.random() * (dummyLength - 1));
+  return {
+    familyName: dummyNameTemplate[familyDummyIndex].familyName,
+    firstName: dummyNameTemplate[firstDummyIndex].firstName,
+    familyNameKana: dummyNameTemplate[familyDummyIndex].familyNameKana,
+    firstNameKana: dummyNameTemplate[firstDummyIndex].firstNameKana
+  }
+}
